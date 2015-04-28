e8819c42687a01ee580f30ff0f1aed57baa269e2
Kento <_k_e_n_t_o_@bitbucket.org>
2015-04-28 16:18:00 -0500

    Changing image every 5 seconds

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 /* We are going to start writing our custom javascript here */
 
+$(document).ready(function(){
+
+    var banner = document.getElementById("banner");
+    
+    var i = 1;
+    window.setInterval(function(){
+	//i = (i=4) ? 1 : i;
+	console.log("img/banner"+ i +".jpg");
+	banner.style.backgroundImage = "url(img/banner"+ i +"c.jpg)";
+	i++;
+	i = (i==4) ? 1 : i;
+    }, 5000);
+    
+ });
+
+
+
+
 
