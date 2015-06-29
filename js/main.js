265d5082db81f5efc90f0765fd1a7e92cefb0abd
kevin17 <kevinalmeyda17@gmail.com>
2015-06-29 00:45:18 -0500

    validacion de formulario js

--- js/main.js
+++ js/main.js
@@ -13,8 +13,30 @@ $(document).ready(function(){
 	i = (i==4) ? 1 : i;
     }, 5000);
     
- });
+ });*/
 
+function enviar() {
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var edad = document.getElementById("edad").value;
+
+	if (nombre == "") {
+		alert("Debes escribir el Nombre");
+		return;
+	}
+	if (apellido == "") {
+		alert("Debes escribir el Apellido");
+		return;
+	}
+	if (edad == "") {
+		alert("Debes escribir la Edad");
+		return;
+	}
+	if (edad != parseInt(edad)) {
+		alert("Ingrese un numero en Edad");
+		return;
+	}
+}
 
 
 
